10b841f00b3527e2c5273e615544a3e1dd8061b7
ChloeYiuSoftwire <chloe.yiu@softwire.com>
2023-06-28 11:53:15 +0100

    Passed unit test for Knight Movement

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,13 +1,38 @@
-import Piece from './piece';
-import Player from '../player';
-import Board from '../board';
+import Piece from "./piece";
+import Player from "../player";
+import Board from "../board";
+import Square from "../square";
 
 export default class Knight extends Piece {
-    public constructor(player: Player) {
-        super(player);
-    }
+  public constructor(player: Player) {
+    super(player);
+  }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
-    }
+  public getAvailableMoves(board: Board) {
+    let location = board.findPiece(this);
+    let moves: Square[] = [];
+    let movements = [
+      [2, 1],
+      [1, 2],
+      [-1, 2],
+      [-2, 1],
+      [2, -1],
+      [1, -2],
+      [-1, -2],
+      [-2, -1],
+    ];
+    movements.forEach((movement) => {
+      if (
+        location.row + movement[0] >= 0 &&
+        location.row + movement[0] <= 7 &&
+        location.col + movement[1] >= 0 &&
+        location.col + movement[1] <= 7
+      ) {
+        moves.push(
+          new Square(location.row + movement[0], location.col + movement[1])
+        );
+      }
+    });
+    return moves;
+  }
 }
