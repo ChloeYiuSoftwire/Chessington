830000df143dbecee2101ffff027ebb71b4033c5
ChloeYiuSoftwire <chloe.yiu@softwire.com>
2023-06-28 13:33:42 +0100

    Passed Rooks can move through pieces

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,26 +1,42 @@
-import Piece from './piece';
-import Player from '../player';
-import Board from '../board';
-import Square from '../square';
+import Piece from "./piece";
+import Player from "../player";
+import Board from "../board";
+import Square from "../square";
 
 export default class Rook extends Piece {
-    public constructor(player: Player) {
-        super(player);
+  public constructor(player: Player) {
+    super(player);
+  }
+
+  public getAvailableMoves(board: Board) {
+    let location = board.findPiece(this);
+    let moves: Square[] = [];
+
+    for (let col = location.col - 1; col >= 0; col--) {
+      if (board.getPiece(new Square(location.row, col)) !== undefined) {
+        break;
+      }
+      moves.push(new Square(location.row, col));
+    }
+    for (let col = location.col + 1; col <= 7; col++) {
+      if (board.getPiece(new Square(location.row, col)) !== undefined) {
+        break;
+      }
+      moves.push(new Square(location.row, col));
+    }
+    for (let row = location.row - 1; row >= 0; row--) {
+      if (board.getPiece(new Square(row, location.col)) !== undefined) {
+        break;
+      }
+      moves.push(new Square(row, location.col));
+    }
+    for (let row = location.row + 1; row <= 7; row++) {
+      if (board.getPiece(new Square(row, location.col)) !== undefined) {
+        break;
+      }
+      moves.push(new Square(row, location.col));
     }
 
-    public getAvailableMoves(board: Board) {
-        let location = board.findPiece(this)
-        let moves = [];
-        for (let col = 0; col < 8; col++) {
-            if (col !== location.col) {
-                moves.push(new Square(location.row, col))
-            }
-        }
-        for (let row = 0; row < 8; row++) {
-            if (row !== location.row) {
-                moves.push(new Square(row, location.col))
-            }
-        }
-        return moves
-    }
+    return moves;
+  }
 }
