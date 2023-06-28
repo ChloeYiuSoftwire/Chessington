fefa38b70942c46ed616cc137f0987120faebe20
ChloeYiuSoftwire <chloe.yiu@softwire.com>
2023-06-28 13:39:51 +0100

    Passed bishop moving through pieces test

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -12,28 +12,48 @@ export default class Bishop extends Piece {
     let location = board.findPiece(this);
     let moves = [];
     for (let num = 1; num < 8; num++) {
-      if (location.row + num >= 8 || location.col + num >= 8) {
+      const targetSquare = new Square(location.row + num, location.col + num);
+      if (
+        location.row + num >= 8 ||
+        location.col + num >= 8 ||
+        board.getPiece(targetSquare) !== undefined
+      ) {
         break;
       }
-      moves.push(new Square(location.row + num, location.col + num));
+      moves.push(targetSquare);
     }
     for (let num = 1; num < 8; num++) {
-      if (location.row - num < 0 || location.col + num >= 8) {
+      const targetSquare = new Square(location.row - num, location.col + num);
+      if (
+        location.row - num < 0 ||
+        location.col + num >= 8 ||
+        board.getPiece(targetSquare) !== undefined
+      ) {
         break;
       }
-      moves.push(new Square(location.row - num, location.col + num));
+      moves.push(targetSquare);
     }
     for (let num = 1; num < 8; num++) {
-      if (location.row + num >= 8 || location.col - num < 0) {
+      const targetSquare = new Square(location.row + num, location.col - num);
+      if (
+        location.row + num >= 8 ||
+        location.col - num < 0 ||
+        board.getPiece(targetSquare) !== undefined
+      ) {
         break;
       }
-      moves.push(new Square(location.row + num, location.col - num));
+      moves.push(targetSquare);
     }
     for (let num = 1; num < 8; num++) {
-      if (location.row - num < 0 || location.col - num < 0) {
+      const targetSquare = new Square(location.row - num, location.col - num);
+      if (
+        location.row - num < 0 ||
+        location.col - num < 0 ||
+        board.getPiece(targetSquare) !== undefined
+      ) {
         break;
       }
-      moves.push(new Square(location.row - num, location.col - num));
+      moves.push(targetSquare);
     }
     return moves;
   }
