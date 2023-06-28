901e9a69c88b1703f79bf1458ed457f76e6bfb53
ChloeYiuSoftwire <chloe.yiu@softwire.com>
2023-06-28 13:41:51 +0100

    Passed Queen moving through pieces test

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -13,40 +13,75 @@ export default class Queen extends Piece {
     let moves = [];
     // bishop part
     for (let num = 1; num < 8; num++) {
-      if (location.row + num >= 8 || location.col + num >= 8) {
+      const targetSquare = new Square(location.row + num, location.col + num);
+      if (
+        location.row + num >= 8 ||
+        location.col + num >= 8 ||
+        board.getPiece(targetSquare) !== undefined
+      ) {
         break;
       }
-      moves.push(new Square(location.row + num, location.col + num));
+      moves.push(targetSquare);
     }
     for (let num = 1; num < 8; num++) {
-      if (location.row - num < 0 || location.col + num >= 8) {
+      const targetSquare = new Square(location.row - num, location.col + num);
+      if (
+        location.row - num < 0 ||
+        location.col + num >= 8 ||
+        board.getPiece(targetSquare) !== undefined
+      ) {
         break;
       }
-      moves.push(new Square(location.row - num, location.col + num));
+      moves.push(targetSquare);
     }
     for (let num = 1; num < 8; num++) {
-      if (location.row + num >= 8 || location.col - num < 0) {
+      const targetSquare = new Square(location.row + num, location.col - num);
+      if (
+        location.row + num >= 8 ||
+        location.col - num < 0 ||
+        board.getPiece(targetSquare) !== undefined
+      ) {
         break;
       }
-      moves.push(new Square(location.row + num, location.col - num));
+      moves.push(targetSquare);
     }
     for (let num = 1; num < 8; num++) {
-      if (location.row - num < 0 || location.col - num < 0) {
+      const targetSquare = new Square(location.row - num, location.col - num);
+      if (
+        location.row - num < 0 ||
+        location.col - num < 0 ||
+        board.getPiece(targetSquare) !== undefined
+      ) {
         break;
       }
-      moves.push(new Square(location.row - num, location.col - num));
+      moves.push(targetSquare);
     }
     // rook part
-    for (let col = 0; col < 8; col++) {
-      if (col !== location.col) {
-        moves.push(new Square(location.row, col));
+    for (let col = location.col - 1; col >= 0; col--) {
+      if (board.getPiece(new Square(location.row, col)) !== undefined) {
+        break;
       }
+      moves.push(new Square(location.row, col));
     }
-    for (let row = 0; row < 8; row++) {
-      if (row !== location.row) {
-        moves.push(new Square(row, location.col));
+    for (let col = location.col + 1; col <= 7; col++) {
+      if (board.getPiece(new Square(location.row, col)) !== undefined) {
+        break;
       }
+      moves.push(new Square(location.row, col));
     }
+    for (let row = location.row - 1; row >= 0; row--) {
+      if (board.getPiece(new Square(row, location.col)) !== undefined) {
+        break;
+      }
+      moves.push(new Square(row, location.col));
+    }
+    for (let row = location.row + 1; row <= 7; row++) {
+      if (board.getPiece(new Square(row, location.col)) !== undefined) {
+        break;
+      }
+      moves.push(new Square(row, location.col));
+    }
+
     return moves;
   }
 }
