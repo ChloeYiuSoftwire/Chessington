f069147389daa5a38336421fb0942905528f0a2d
ChloeYiuSoftwire <chloe.yiu@softwire.com>
2023-06-28 11:33:25 +0100

    Passed test for bishop movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,13 +1,40 @@
-import Piece from './piece';
-import Player from '../player';
-import Board from '../board';
+import Piece from "./piece";
+import Player from "../player";
+import Board from "../board";
+import Square from "../square";
 
 export default class Bishop extends Piece {
-    public constructor(player: Player) {
-        super(player);
-    }
+  public constructor(player: Player) {
+    super(player);
+  }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+  public getAvailableMoves(board: Board) {
+    let location = board.findPiece(this);
+    let moves = [];
+    for (let num = 1; num < 8; num++) {
+      if (location.row + num >= 8 || location.col + num >= 8) {
+        break;
+      }
+      moves.push(new Square(location.row + num, location.col + num));
     }
+    for (let num = 1; num < 8; num++) {
+      if (location.row - num < 0 || location.col + num >= 8) {
+        break;
+      }
+      moves.push(new Square(location.row - num, location.col + num));
+    }
+    for (let num = 1; num < 8; num++) {
+      if (location.row + num >= 8 || location.col - num < 0) {
+        break;
+      }
+      moves.push(new Square(location.row + num, location.col - num));
+    }
+    for (let num = 1; num < 8; num++) {
+      if (location.row - num < 0 || location.col - num < 0) {
+        break;
+      }
+      moves.push(new Square(location.row - num, location.col - num));
+    }
+    return moves;
+  }
 }
